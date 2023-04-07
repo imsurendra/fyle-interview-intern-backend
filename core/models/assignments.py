19cb034df2aeadc5b307a4f4f15f3a08d781e609
imsurendra <ss2971784@gmail.com>
2023-04-08 04:53:17 +0530

    fixed assignment resumit error

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -65,6 +65,23 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
+                                    'only a draft assignment can be submitted')
+
+        assignment.teacher_id = teacher_id
+        assignment.state = AssignmentStateEnum.SUBMITTED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
+    def grade(cls, _id, teacher_id, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
+        assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
+                                    'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -75,3 +92,7 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_by_teacher(cls, student_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
