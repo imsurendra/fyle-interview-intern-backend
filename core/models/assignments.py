73ce0b0c631fce75697abcd7e4daaf244618866d
imsurendra <ss2971784@gmail.com>
2023-04-08 06:50:16 +0530

    Added 2 missing APIs

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -75,17 +75,19 @@ class Assignment(db.Model):
         return assignment
 
     @classmethod
-    def grade(cls, _id, teacher_id, principal: Principal):
+    def grade_assignment(cls, _id, grade, principal: Principal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
-        assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
-        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
-                                    'only a draft assignment can be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                                                                'Only a submitted assignment can be graded')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        possibleGrades = [GradeEnum.A, GradeEnum.B, GradeEnum.C, GradeEnum.D]
+        for curGrade in possibleGrades:
+            assertions.assert_valid(grade == curGrade, 'Grade can only be given from available grades')
 
-        assignment.teacher_id = teacher_id
-        assignment.state = AssignmentStateEnum.SUBMITTED
-        db.session.flush()
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()      
 
         return assignment
 
@@ -94,5 +96,5 @@ class Assignment(db.Model):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls, student_id):
+    def get_assignments_by_teacher(cls, teacher_id):
         return cls.filter(cls.teacher_id == teacher_id).all()
